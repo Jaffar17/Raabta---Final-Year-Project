7bba23683fbd1b4930c4e27607328bfc2796adbb
Jaffar Abbas <abbasjaffar28@gmail.com>
2023-01-30 12:33:43 +0500

    test commit reverted

--- lib/User/profile_user.dart
+++ lib/User/profile_user.dart
@@ -11,29 +11,27 @@ class ProfileUser extends StatefulWidget {
 class _ProfileUserState extends State<ProfileUser> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SafeArea(
-          child: Scaffold(
-              body: Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(image: DecorationImage(
-                    image: AssetImage("assets/images/Background.jpeg"), fit: BoxFit.cover)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 16.0, right: 16.0,  bottom: 70.0),
-                  child: Image.asset(
-                    "assets/images/ProfileHeading.png",
-                     width: 150,
-                     height: 200,
-                  ),
+    return SafeArea(
+        child: Scaffold(
+            body: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(image: DecorationImage(
+                  image: AssetImage("assets/images/Background.jpeg"), fit: BoxFit.cover)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16.0, right: 16.0,  bottom: 70.0),
+                child: Image.asset(
+                  "assets/images/ProfileHeading.png",
+                   width: 150,
+                   height: 200,
                 ),
-                ],
-              ),),
-      )),
-    );
+              ),
+              ],
+            ),),
+    ));
   }
 }
